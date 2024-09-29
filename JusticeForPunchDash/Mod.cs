02b5151b20284f1986272525a25c326442ecb1fe
sneakykestrel <141267380+sneakykestrel@users.noreply.github.com>
2024-09-29 22:13:31 +0100

    Add project files.

---
+++ JusticeForPunchDash/Mod.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx.Configuration;
+using BepInEx.Unity.Mono;
+using BepInEx;
+using HarmonyLib;
+using Projectiles;
+using UnityEngine;
+using AimAssist;
+using Enemy;
+using System;
+
+namespace JusticeForPunchDash
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginversion)]
+    public class Mod : BaseUnityPlugin
+    {
+        public const string pluginGuid = "kestrel.iamyourbeast.justiceforpunchdash";
+        public const string pluginName = "Justice For Punch Dash";
+        public const string pluginversion = "1.0.0";
+
+        public static bool successfulHit = false;
+
+        public void Awake() {
+            Logger.LogInfo("Hiiiiiiiiiiii :3");
+            Harmony harmony = new Harmony(pluginGuid);
+            harmony.PatchAll();
+        }
+    }
+
+    //This patch is essentially just the important parts of the original demo Assembly-CSharp UseEquipment function reimplemented. this does mean some things will run twice but that shouldn't cause too many issues. i hope
+    [HarmonyPatch(typeof(PlayerMeleeArmed), nameof(PlayerMeleeArmed.UseEquipment))]
+    public class TestPatchMelee
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ref WeaponPickup ___pickup) {
+
+            bool flag = false;
+            Vector3 pos = Vector3.zero;
+            Vector3 goal = Vector3.zero;
+            bool flag3 = true;
+
+            if (___pickup && ___pickup.GetWeaponDetails().GetProjectilePrefab().GetComponent<SpherecastProjectile>().GetCollisionType() == SpherecastProjectile.CollisionType.AllTargets) {
+                flag3 = false;
+            }
+
+            AimTarget highlightedTarget = GameManager.instance.player.GetAimManager().GetHighlightedTarget();
+
+            if (highlightedTarget) {
+                pos = highlightedTarget.GetCenter();
+                Enemy.Enemy enemy = highlightedTarget.GetEnemy();
+                if (enemy && flag3 && enemy is EnemyHuman) {
+                    flag = true;
+                    goal = enemy.transform.position + Vector3.Normalize(GameManager.instance.player.GetMovementScript().transform.position - enemy.transform.position) * 1.5f;
+                }
+
+
+                if (Mod.successfulHit) {
+                    float duration = 0.125f;
+                    GameManager.instance.player.GetLookScript().TriggerTimedLockOn(pos, duration, true);
+
+                    if (flag) {
+                        GameManager.instance.player.GetMovementScript().TriggerTimedLockOn(goal, duration);
+                    }
+                }
+            }
+        }
+
+    }
+
+    [HarmonyPatch(typeof(PlayerWeaponArmed), nameof(PlayerWeaponArmed.UseEquipment))]
+    public class WeaponHit
+    {
+        //We know that the previous patch will be run after UseEquipment has been called, so this is a good enough solution that avoids calling base class methods from within a patch (nasty)
+        [HarmonyPostfix]
+        public static void Postfix(bool __result) {
+            Mod.successfulHit = __result;
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Justice For Punch Dash mod for I Am Your Beast
+
+In the IAYB demo, hitting a punch on one enemy while aiming at a different enemy would teleport you to that second enemy. This has since been patched because the developers hate fun. 
+
+This mod restores that bug!
+
+# Installation
+
+!!! You need [Bepinex 6](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) for mono !!!
+(if you have no idea what the versions mean try BepInEx-Unity.Mono-win-x64-6.0.0-pre.2 and it might work. maybe)
+
+Once bepinex is installed, put the dll into the BepInEx/plugins folder in the game's root directory.
+
+have fun :3
+
+![](https://files.catbox.moe/8p6xn8.jpg)
